4aa7b4fb5ad16d0178bbf48c952bbde5ad020cd1
aryaanadam55 <145668673+aryaanadam55@users.noreply.github.com>
2023-10-05 16:21:45 +0530

    Add files via upload

---
+++ postlist.py
@@ -0,0 +1,9 @@
+s=[1,2,4,-5,6,8,-3,9,32,-27]
+list=[]
+print("the numbers list are")
+for i in s:
+    if i>0:
+        list.append(i)
+print(list)
+    
+
---
+++ vowels.py
@@ -0,0 +1,5 @@
+text=(input("enter the string:"))
+for char in text:
+    if char.lower() in "aeiou":
+        print(char)
+  
